55cb64a3fa20e96b939700f0a2be18d39fb1521e
Anurag Singh <sa.anuragsingh@gmail.com>
2023-05-29 15:07:39 +0530

    feat: create openai api stream

--- src/env.mjs
+++ src/env.mjs
@@ -8,6 +8,9 @@ export const env = createEnv({
    */
   server: {
     NODE_ENV: z.enum(["development", "test", "production"]),
+    OPENAI_API_KEY: z.string().min(10),
+    SUPABASE_URL: z.string().min(10),
+    SUPABASE_KEY: z.string().min(10),
   },
 
   /**
@@ -25,6 +28,9 @@ export const env = createEnv({
    */
   runtimeEnv: {
     NODE_ENV: process.env.NODE_ENV,
+    OPENAI_API_KEY: process.env.OPENAI_API_KEY,
+    SUPABASE_URL: process.env.SUPABASE_URL,
+    SUPABASE_KEY: process.env.SUPABASE_KEY,
     // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
   },
   /**
