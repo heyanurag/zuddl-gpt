328a4e84bf2f1b2025f77408c01984bf1ee183ee
Umang Thadani <thadaniumang@outlook.com>
2023-06-01 17:28:57 +0530

    feat: rate limit middleware

--- src/env.mjs
+++ src/env.mjs
@@ -12,6 +12,10 @@ export const env = createEnv({
     SUPABASE_URL: z.string().min(10),
     SUPABASE_KEY: z.string().min(10),
     RESPONSE_MAX_TOKENS: z.number().int().min(9).max(1024),
+    UPSTASH_REDIS_REST_URL: z.string().min(10),
+    UPSTASH_REDIS_REST_KEY: z.string().min(10),
+    UPSTASH_NUMBER_OF_REQUESTS: z.number().int().min(1).max(5),
+    UPSTASH_REQUESTS_INTERVAL: z.number().int().min(3).max(10),
   },
 
   /**
@@ -33,6 +37,10 @@ export const env = createEnv({
     SUPABASE_URL: process.env.SUPABASE_URL,
     SUPABASE_KEY: process.env.SUPABASE_KEY,
     RESPONSE_MAX_TOKENS: Number(process.env.RESPONSE_MAX_TOKENS),
+    UPSTASH_REDIS_REST_URL: process.env.UPSTASH_REDIS_REST_URL,
+    UPSTASH_REDIS_REST_KEY: process.env.UPSTASH_REDIS_REST_KEY,
+    UPSTASH_NUMBER_OF_REQUESTS: Number(process.env.UPSTASH_NUMBER_OF_REQUESTS),
+    UPSTASH_REQUESTS_INTERVAL: Number(process.env.UPSTASH_REQUESTS_INTERVAL),
     // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
   },
   /**
---
+++ src/lib/middleware.ts
@@ -0,0 +1,31 @@
+import { type NextRequest, NextResponse } from "next/server";
+import { Ratelimit } from "@upstash/ratelimit";
+import { Redis } from "@upstash/redis";
+import { env } from "@/env.mjs";
+
+const redis = new Redis({
+  url: env.UPSTASH_REDIS_REST_URL,
+  token: env.UPSTASH_REDIS_REST_KEY,
+});
+
+const ratelimit = new Ratelimit({
+  redis: redis,
+  limiter: Ratelimit.slidingWindow(
+    env.UPSTASH_NUMBER_OF_REQUESTS,
+    `${env.UPSTASH_REQUESTS_INTERVAL} s`
+  ),
+});
+
+export default async function middleware(
+  request: NextRequest
+): Promise<Response | undefined> {
+  const ip = request.ip ?? "127.0.0.1";
+  const { success } = await ratelimit.limit(ip);
+  return success
+    ? NextResponse.next()
+    : NextResponse.redirect(new URL("/blocked", request.url));
+}
+
+export const config = {
+  matcher: "/",
+};
