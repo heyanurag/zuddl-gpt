94928aff0571f64a03c54908a2edc4e24071d75b
Anurag Singh <sa.anuragsingh@gmail.com>
2023-06-01 18:30:31 +0530

    fix: move middleware location

--- src/middleware.ts
+++ src/middleware.ts
@@ -12,19 +12,22 @@ const ratelimit = new Ratelimit({
   redis: redis,
   limiter: Ratelimit.slidingWindow(
     env.UPSTASH_NUMBER_OF_REQUESTS,
-    `${env.UPSTASH_REQUESTS_INTERVAL} s`
+    `${env.UPSTASH_REQUESTS_INTERVAL}s`
   ),
 });
 
 export default async function middleware(
   request: NextRequest
 ): Promise<Response | undefined> {
-  console.log("middleware");
   const ip = request.ip ?? "127.0.0.1";
-  console.log("ip", ip);
-  console.log("upstash_url", env.UPSTASH_REDIS_REST_URL);
+
   const { success } = await ratelimit.limit(ip);
+
   return success
     ? NextResponse.next()
     : NextResponse.redirect(new URL("/blocked", request.url));
 }
+
+export const config = {
+  matcher : "/",
+};
