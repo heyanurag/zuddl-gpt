65ed88c4ba4f22a741d492aa4f002c74fb0457d8
Umang Thadani <thadaniumang@outlook.com>
2023-06-01 17:32:49 +0530

    feat: blocked page

---
+++ src/pages/blocked.tsx
@@ -0,0 +1,9 @@
+const Blocked = () => {
+  return (
+    <div className="flex flex-col justify-center items-center h-full w-full">
+      <h1 className="text-2xl font-bold text-primary">Too many requests. Please try again after sometime</h1>
+    </div>
+  );
+}
+
+export default Blocked;
