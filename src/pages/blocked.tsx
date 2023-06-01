36b360370dd7f8b1c6559078721751a6cb311a54
Umang Thadani <thadaniumang@outlook.com>
2023-06-01 19:02:54 +0530

    improve: blocked page ui

--- src/pages/blocked.tsx
+++ src/pages/blocked.tsx
@@ -1,7 +1,11 @@
+import Image from "next/image";
+
 const Blocked = () => {
   return (
-    <div className="flex flex-col justify-center items-center h-full w-full">
-      <h1 className="text-2xl font-bold text-primary">Too many requests. Please try again after sometime</h1>
+    <div className="flex flex-col justify-center items-center h-screen">
+      <Image src="/slow-down.png" width={300} height={300} alt="429" />
+      <p className="mt-4 text-2xl font-bold text-primary">Too many requests :&#40; </p>
+      <p className="mt-2 text-lg font-medium text-gray-600">Please try again after sometime</p>
     </div>
   );
 }
